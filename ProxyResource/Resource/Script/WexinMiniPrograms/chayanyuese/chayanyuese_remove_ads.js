64d2b9fab9f4bcc9c029a73101a01f06d9b8d679
Savantank <60205351+Savantank@users.noreply.github.com>
2024-08-22 01:05:48 +0800

    Delete ProxyResource directory

--- ProxyResource/Resource/Script/WexinMiniPrograms/chayanyuese/chayanyuese_remove_ads.js
+++
@@ -1,9 +0,0 @@
-let obj=JSON.parse($response.body);
-delete obj.data.INDEX_SLOT_01;
-delete obj.data.INDEX_SLOT_02;
-obj.data?.INDEX_TOP_BANNER?.contents?.forEach(item => {
-  delete item.bubble; 
-  delete item.figure; 
-  item.value = "https://raw.githubusercontent.com/luestr/ProxyResource/main/Resource/Script/WexinMiniPrograms/chayanyuese/cyys1.jpg";
-});
-$done({body: JSON.stringify(obj)});
